6c08ab11d6cfb7d2dafa1cedf636a44d25719ec6
laperzyna <90733086+laperzyna@users.noreply.github.com>
2023-09-26 08:59:10 -0700

    cleaned code and added friends

--- js/script.js
+++ js/script.js
@@ -5,26 +5,6 @@ function hide() {
   document.getElementById("mySidebar").style.width = "0";
 }
 
-// Function to handle scroll events
-function handleScroll() {
-  const elementsToReveal = document.querySelectorAll('.reveal');
-  const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-  const scrollPosition = window.scrollY;
-
-  elementsToReveal.forEach((element, index) => {
-    const rect = element.getBoundingClientRect();
-    const elementTop = rect.top + scrollPosition;
-
-    if (elementTop < windowHeight) {
-      setTimeout(() => {
-        element.classList.add('visible');
-      }, index * 1000);
-    }
-  });
-}
-
-window.addEventListener('scroll', handleScroll);
-
 // Red Display
 document.getElementById("denied").addEventListener("click", function() {
   var element = this;
